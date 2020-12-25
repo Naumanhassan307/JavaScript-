023fa7df80af0899cd5dab35a4b0b0ef5c8c059b
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-24 20:31:02 -0800

    visa eligibility finder work start

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 // start first JavaScriot program
-console.log("Hello World");
-//<------------------>
-let a=1;
-let b=2;
+// console.log("Hello World");
+// //<------------------>
+// let a=1;
+// let b=2;
 
-const c=a+b;
-console.log(c)
+// const c=a+b;
+// console.log(c)
 
-//<------------------>
-
-
-let q=307;
-console.log(q)
 //<------------------>
 
-let fname="Nauman";
-let sname="Hassan";
 
-console.log(fname + " "+ sname +"   " + q)
+// let q=307;
+// console.log(q)
 //<------------------>
+
+// let fname="Nauman";
+// let sname="Hassan";
+
+// console.log(fname + " "+ sname +"   " + q)
+//<------------------>
+
+//<------------------>
+console.log("|-----------------------------------|")
+console.log("      Visa Eligibility Finder        ")
+console.log("|-----------------------------------|")
+const age=19;
+
+if (age >=50){
+    console.log("You are senior citizen.")
+} else if(age >18){
+    console.log("You are young.")
+}else {
+    console.log("You are child.")
+}
+
