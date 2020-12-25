f1fb8b83aa8c8d3109873871bae4a71475294f3b
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-25 12:12:20 -0800

    basic grade cal complete

--- Grade-cal.js
+++ Grade-cal.js
@@ -1,11 +1,46 @@
 //<-------Grade calculator---------->
 
-const marks=89;
+const marks=90;
 
 let pass=false;
 let fail=true;
 
 if (marks >=90){
     pass=true;
+    fail=false;
     console.log("Congratulation! You passed your Exam and obtained grade  A+ ")
+}else
+if (marks >=80){
+    pass=true;
+    fail=false;
+    console.log("Congratulation! You passed your Exam and obtained grade  A ")
+}
+}else
+if (marks >=70){
+    pass=true;
+    fail=false;
+    console.log("Congratulation! You passed your Exam and obtained grade  B ")
+}
+}else
+if (marks >=60){
+    pass=true;
+    fail=false;
+    console.log("Congratulation! You passed your Exam and obtained grade  C ")
+}
+}else
+if (marks >=50){
+    pass=true;
+    fail=false;
+    console.log("Congratulation! You passed your Exam and obtained grade  D ")
+}
+}else
+if (marks >=40){
+    pass=true;
+    fail=false;
+    console.log("Congratulation! You passed your Exam and obtained grade  E ")
+}
+} else {
+    pass=false;
+    fail=true;
+    console.log("You failed your Exam and obtained grade  F ")
 }
