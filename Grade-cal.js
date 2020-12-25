3744babcc043ee7c1c4ea99dda4800c4e627d706
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-25 12:00:45 -0800

    A+ grade is complete

---
+++ Grade-cal.js
@@ -0,0 +1,11 @@
+//<-------Grade calculator---------->
+
+const marks=89;
+
+let pass=false;
+let fail=true;
+
+if (marks >=90){
+    pass=true;
+    console.log("Congratulation! You passed your Exam and obtained grade  A+ ")
+}
