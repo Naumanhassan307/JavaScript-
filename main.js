fc03950a434be29a967d80391414221c24fc2449
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-24 19:55:20 -0800

    hello world

---
+++ main.js
@@ -0,0 +1,15 @@
+// start first JavaScriot program
+console.log("Hello World");
+//<------------------>
+let a=1;
+let b=2;
+
+const c=a+b;
+console.log(c)
+
+//<------------------>
+
+
+let a=3;
+console.log(a)
+//<------------------>
