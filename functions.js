5d5f32c5b36a22df38d50a3d3f88cc72830f4f6e
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-29 11:43:59 -0800

    complete add function

--- functions.js
+++ functions.js
@@ -11,4 +11,12 @@ function myFun() {
 }
 myFun();
 
-// 
+// function for addition
+
+function add(value1=0, value2=0) {
+    const result=value1 + value2;
+    console.log(result)
+    return console.log(result)
+}
+
+add(1);
